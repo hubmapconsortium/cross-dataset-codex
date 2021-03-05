113d0f18ca762dd7140229422996062be559e570
Sean <Sean.Donahue.5311@gmail.com>
2021-03-05 12:34:07 -0500

    Handling multiple cell regions and stitched images, reading from hdf filestore, instead of csv

--- bin/parse_sprm_output.py
+++ bin/parse_sprm_output.py
@@ -11,60 +11,79 @@ from cross_dataset_common import find_files, get_tissue_type, create_minimal_dat
 
 from concurrent.futures import ThreadPoolExecutor
 
+def outer_join(df1:pd.DataFrame, df2:pd.DataFrame):
+    return df1.merge(df2, how='outer')
+
+def get_image_regions(keys):
+    return set([key.split('/')[-1] for key in keys])
+
+def get_quantification_keys(statistic, cell_region, image_region):
+    return f"/{statistic}/channel/{cell_region}/expressions.ome.tiff/stitched/{image_region}"
+
+def get_cluster_keys(image_region):
+    return f"/cluster/cells/expressions.ome.tiff/stitched/{image_region}"
+
 def get_dataset(dataset_directory: Path) -> str:
     return dataset_directory.stem
 
+def get_cluster_assignments(cluster_df, region):
 
-def get_tile_id(file: Path) -> str:
-    return str(file.stem).split('.')[0]
-
-
-def get_cluster_assignments(dataset_directory, tile):
-    cluster_file = dataset_directory / Path(f"sprm_outputs/{tile}.ome.tiff-cell_cluster.csv")
-    cluster_df = pd.read_csv(cluster_file)
-
-    cluster_assignments_list = [",".join([f"KMeans-{column.split('[')[1].split(']')[0]}-{dataset_directory.stem}-{tile}-" \
+    cluster_assignments_list = [",".join([f"KMeans-{column.split('[')[1].split(']')[0]}-{dataset_directory.stem}-{region}-" \
                                           f"{cluster_df.at[i, column]}" for column in cluster_df.columns[1:]]) for i in
                                 cluster_df.index]
 
     return cluster_assignments_list
 
 
-def flatten_df(df: pd.DataFrame, statistic: str) -> pd.DataFrame:
+def flatten_df(df: pd.DataFrame) -> pd.DataFrame:
     on_columns = [column for column in df.columns if column not in ['cell_id', 'clusters', 'dataset', 'organ', 'tile',
                                                                     'modality'] and 'DAPI' not in column]
     dict_list = [
-        {'q_var_id': column, 'q_cell_id': df.at[i, 'cell_id'], 'statistic': statistic, 'value': df.at[i, column]}
+        {'q_var_id': column.split('_')[2], 'q_cell_id': df.at[i, 'cell_id'], 'statistic': column.split('_')[1], 'region':
+            column.split('_')[0], 'value': df.at[i, column]}
         for i in df.index for column in on_columns]
 
-    return dict_list
+    return pd.DataFrame(dict_list)
+
+def get_quantification_df(hdf_file, statistic, cell_region, image_region):
+    quantification_key = get_quantification_keys(statistic, cell_region, image_region)
+    quantification_df = pd.read_hdf(hdf_file, quantification_key)
+    new_columns = [f"{cell_region}_{statistic}_{column}" if column != "ID" else column for column in
+                   quantification_df.columns]
+    quantification_df.columns = new_columns
+    return quantification_df
+
+def get_region_df(hdf_file, image_region):
+    statistics = ["mean", "total", "covar"]
+    cell_regions = ["cell", "nucleus"]
+    cluster_key = get_cluster_keys(image_region)
+    cluster_df = pd.read_hdf(hdf_file, cluster_key)
+    quantification_dfs = [get_quantification_df(hdf_file, statistic, cell_region, image_region) for statistic in
+                          statistics for cell_region in cell_regions]
+    region_df = reduce(outer_join, quantification_dfs)
+    region_df["clusters"] = pd.Series(get_cluster_assignments(cluster_df, image_region))
+    region_df['cell_id'] = pd.Series(
+        [get_spatial_cell_id(dataset, image_region, mask_index) for mask_index in region_df['ID']], index=region_df.index)
+
+    return region_df
 
 
-def stitch_dfs(data_file: str, dataset_directory: Path, nexus_token: str) -> pd.DataFrame:
+def annotate_df(dataset_directory: Path, nexus_token: str) -> pd.DataFrame:
     modality = 'codex'
     dataset = get_dataset(dataset_directory)
     tissue_type = get_tissue_type(dataset, nexus_token)
 
-    csv_files = [file for file in find_files(dataset_directory, data_file) if 'R' in file.stem]
+    hdf_file = dataset_directory / Path('sprm-outputs/out.hdf5')
+    hdf_keys = hdf_file.keys()
+    image_regions = get_image_regions(hdf_keys)
 
-    tile_ids_and_dfs = [(get_tile_id(csv_file), pd.read_csv(csv_file)) for csv_file in csv_files]
+    region_dfs = [get_region_df(hdf_file, region) for region in image_regions]
 
-    for id_and_df in tile_ids_and_dfs:
-        tile_id = id_and_df[0]
-        tile_df = id_and_df[1]
-        tile_df['ID'] = tile_df['ID'].astype(str)
-        tile_df['tile'] = tile_id
-        tile_df['cell_id'] = pd.Series(
-            [get_spatial_cell_id(dataset, tile_id, mask_index) for mask_index in tile_df['ID']], index=tile_df.index)
-        tile_df.drop(['ID'], axis=1, inplace=True)
-        if data_file == '**cell_channel_total.csv':  # If this is the third set of CSVs for this dataset
-            tile_df['clusters'] = pd.Series(get_cluster_assignments(dataset_directory, tile_id), index=tile_df.index)
+    dataset_df = concat_dfs(region_dfs) if len(region_dfs) > 1 else region_dfs[0]
 
-    stitched_df = concat_dfs([id_and_df[1] for id_and_df in tile_ids_and_dfs])
-
-    stitched_df['dataset'] = pd.Series([dataset for i in stitched_df.index], index=stitched_df.index)
-    stitched_df['organ'] = tissue_type
-    stitched_df['modality'] = modality
+    dataset_df['dataset'] = dataset
+    dataset_df['organ'] = tissue_type
+    dataset_df['modality'] = modality
 
     return stitched_df
 
@@ -76,24 +95,10 @@ def concat_dfs(dfs: List[pd.DataFrame]):
     return pd.DataFrame(new_df_list)
 
 
-def outer_join(df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
-    return df_1.merge(df_2, how='outer')
-
-
 def get_dataset_dfs(dataset_directory: Path, nexus_token: str=None) -> (pd.DataFrame, pd.DataFrame):
-    per_cell_data_files = ['**cell_channel_covar.csv', '**cell_channel_mean.csv', '**cell_channel_total.csv']
 
-    stitched_dfs = [stitch_dfs(data_file, dataset_directory, nexus_token) for data_file in per_cell_data_files]
-    statistics = [file.split('_')[2][:-4] for file in per_cell_data_files]
-    df_stats = {statistics[i]:stitched_dfs[i] for i in range(len(stitched_dfs))}
-
-    quant_df_lists = []
-
-    for stat in df_stats:
-        dict_list = flatten_df(df_stats[stat], stat)
-        quant_df_lists.extend(dict_list)
-
-    quant_df = pd.DataFrame(quant_df_lists)
+    dataset_df = annotate_df(dataset_directory, nexus_token)
+    quant_df = flatten_df(dataset_df)
 
     return dataset_df, quant_df
 
@@ -111,7 +116,7 @@ def main(nexus_token: str, dataset_directories: List[Path]):
     quant_df = concat_dfs(quant_dfs)
     cell_df = concat_dfs(dataset_dfs)
 
-    cell_df = cell_df[['cell_id', 'dataset', 'modality', 'organ', 'tile', 'clusters']]
+    cell_df = cell_df[['cell_id', 'dataset', 'modality', 'organ', 'clusters']]
 
     with pd.HDFStore('codex.hdf5') as store:
         store.put('cell', cell_df)
